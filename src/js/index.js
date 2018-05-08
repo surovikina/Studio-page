bb32f50e55270eef6113551530c743f8b245a3b7
surovikina <korol0955559531@gmail.com>
2018-05-09 02:18:31 +0300

    added changing the image in section works for desktop more than 1280px

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 
+(function(){
+
 var mobMenu = document.querySelector('.main-nav__trigger');
 var accordion = document.querySelector('.main-nav__dropdown');
 var mobMenuClickHandler = function(){
@@ -8,3 +10,23 @@ var mobMenuClickHandler = function(){
   accordion.classList.remove('hidden');
 };
 mobMenu.addEventListener('click', mobMenuClickHandler);
+
+var loadImages = function () {
+    var image = document.querySelector('.works__item-img');
+    var imgLarge = document.querySelector('.works__item-img--large');
+  if (window.innerWidth > 1280) {
+
+    image.style.display = 'none';
+    imgLarge.style.display = 'block';
+
+  }
+  else {
+
+      image.style.display = 'block';
+      imgLarge.style.display = 'none';
+  }
+};
+document.addEventListener('DOMContentLoaded', loadImages);
+window.addEventListener('resize', loadImages);
+
+}());
